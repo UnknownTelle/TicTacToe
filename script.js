2655229c14a3c31c112265cbc4d5451a458a1d3e
UnknownTelle <telleabbott@gmail.com>
2020-01-04 18:05:47 +0000

    This page has been changed to improve the overall functionality. Some of the bigger changes:
         - A reshuffle of the code layout
         - A if statement put in the eachTurn function so when a win is declared the game will stop users from a continue of play.
         - endGame function now has a way to determine and display the winner of the game by the use of an if statement
         - startGmae function has been refined, it starts the game and resets
         - The code for the that two buttons have been added

--- script.js
+++ script.js
@@ -2,25 +2,17 @@
 var gameBoard;
 var player1 = 'X';
 var player2 = 'O';
-
+var winner = false;
 const boxes = document.getElementsByClassName("game");
-startGame();
-
-function startGame() {
-    gameBoard = Array.from(document.getElementsByClassName("game"));
-    console.log("ln 11");
-    playOneListener();
-    reset();
-}
-function reset() {
-    for (var i = 0; i < gameBoard.length; i++){
-        boxes[i].innerText = '';
-    }
-}
 <!-- Allowing the users to take terns -->
 function eachTurn(boxID, player) {
-    gameBoard[boxID] = player;
-    document.getElementById(boxID).innerText = player;
+    if (winner) {
+        boxes[i].removeEventListener('click', playerTwo, false);
+        boxes[i].removeEventListener('click', playerOne, false);
+    } else {
+        gameBoard[boxID] = player;
+        document.getElementById(boxID).innerText = player;
+    }
 }
 function playerOne(playOne) {
     eachTurn(playOne.target.id, player1);
@@ -61,29 +53,51 @@ const way2win = [
     [2, 5, 8],
     [0, 4, 8],
     [6, 4, 2]
-]
+];
 function sameString(arr) {
     console.log(arr.every( q => q === arr[0] && q !== '') + " ln 65");
     return arr.every( q => q === arr[0] && q !== '');
 }
 
-function endGame() {
-    window.alert('game over');
+function endGame(win) {
+    if (win[0] === player1){
+        window.alert("Game Over! Player One Wins");
+    } else if (win[0] === player2){
+        window.alert("Game Over! Player Two Wins");
+    } else {
+        window.alert("Game Over! It was a draw");
+    }
 }
 
 function checkForWinner(){
-    let winner = false;
     const grid = gameBoard;
     way2win.forEach(a => {
         console.log("ln 75");
         var gridSequence = [grid[a[0]], grid[a[1]], grid[a[2]]];
-
         console.log(gridSequence + " ln 78");
-
         if(sameString(gridSequence)){
             winner = true;
             endGame(gridSequence);
         }
     });
     return winner;
+}
+<!-- HTML buttons code -->
+function startGame() {
+    winner = false;
+    gameBoard = Array.from(document.getElementsByClassName("game"));
+    playOneListener();
+    for(var i = 0; i < gameBoard.length; i++){
+        boxes[i].innerText = "";
+    }
+}
+function PlayerInfo() {
+    window.alert("Player One: Symbol = X , Light colour = Red \nPlayer Two: Symbol = O , Light colour = Blue");
+}
+function info() {
+    window.alert("How To Play:\nEach player will take it in turns to add a symbol to the playing board." +
+        " If a player is able to get a sequence of three symbols in a row then they win and their playing colour " +
+        "will show on the hue light. If no one is able to get a sequence then the game is a draw." +
+        " \n\nPress the Start Game button to start and reset the game." +
+        "\nThe Player Info button will tell you the symbol and colour each player is.");
 }
